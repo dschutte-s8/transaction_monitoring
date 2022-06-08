fa5f9dc9ae6ea4d51b201621ae8e52df10a0d0d1
Dalton Schutte <dalton.schutte@silenteight.com>
2022-06-08 15:01:56 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Introduction
+
+This is meant to serve as a toolbox or playground for our team to work on a transaction monitoring solution. The hope is that folks will add new tools or techniques that they develop or contribute things that just make the work easier and this will be updated accordingly.
+
+
+# Setup
+
+## PyGraphviz
+Installing pygraphviz first requires installing graphviz before installing pygraphviz via pip.
+
+### Linux
+```sudo apt-get install graphviz graphviz-dev```
+
+### MacOS
+```brew install graphviz```
+
+## AWS
+We will have an AWS environment setup with the datasets pre-loaded into S3 buckets and Neptune setup and ready to go. You will just have to clone the repo and install the packages in the requirements file.
+
+## Local
+If you want to experiment with this locally, you will want to run the get_data.sh script in the setup folder first. Currently (06/06/2021) it only installs a graph containing 10K accounts and 1M transactions.
+
+
+# Overview
+
+## Intro
---
+++ setup/get_data.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Tested on Ubuntu 20.04.4
+
+cd ../data
+
+#########################
+# Retrieve zip archives #
+#########################
+
+echo 'Retrieving data...'
+
+# Retrieves bank v2.1
+# Will do fix later
+# wget --output-document 'v2.1-data.zip' 'https://uce75456bd6bda948a1a03ce79cf.dl.dropboxusercontent.com/zip_download_get/BKFYgiOJ_4nqm7xD3aazQhvr-OKAWAQRRDRlf4-r3CjuGSjOh7NV5a1omJ0vwlwIGIXNaOa4c2ihhB-sucEsEkPi7Gd1gjCj7Fta2jvKbUBfDw?_download_id=78517801269199080525709667045716275192616235283599634396798843723&_notify_domain=www.dropbox.com&dl=1'
+
+# Retrieves 10kvertices-1Medges
+wget --output-document '10Kvertices-1Medges.7z' 'https://www.dropbox.com/sh/l3grpumqfgbxqak/AADsWllVa5CIbgBMcFXKAFhda/10Kvertices-1Medges.7z?dl=0'
+
+echo 'Data retrieved...'
+
+##################
+# Unzip archives #
+##################
+# unzip v2.1-data.zip
+
+echo 'Unzipping...'
+
+unzip v2.1-data.zip
+
+read -p 'Do you mind if p7zip is installed? [y/n]: ' PERMIT_INSTALL
+
+if [ $PERMIT_INSTALL=='y' ];
+then
+	sudo apt-get update
+	yes y | sudo apt-get install p7zip-full
+	7z x 10Kvertices-1Medges.7z
+
+elif [ $PERMIT_INSTALL=='n' ];
+then
+
+	echo 'Permission denied, skipping install. You will need to unzip the 7zip file manually.'
+
+else
+
+	echo Invalid input $PERMIT_INSTALL, interrupting execution...
+	exit 0
+
+fi
+
+echo 'Unzipping complete...'
+
+# Iteratively unarchive .gz files in v2.1
+# Will do later
+
+
+# Delete zip archives
+
+echo 'Cleaning up...'
+
+rm 10Kvertices-1Medges.7z
+
+echo '[PROCESS COMPLETED]'
